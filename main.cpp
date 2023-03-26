cf5eb20611ba3a8d3e8fc543c53f3cb61be03e8c
k2supra <andrijkorolevic@gmail.com>
2023-03-26 17:26:36 +0300

    .

--- main.cpp
+++ main.cpp
@@ -6,34 +6,25 @@ using namespace std;
 
 int main()
 {
-    ifstream file1("file1.txt");
-    ifstream file2("file2.txt");
-    string line1, line2;
-    int lineNumber = 1;
+    string file_name = "file1_5.txt";
 
-    while (getline(file1, line1) && getline(file2, line2))
+    ifstream input_file(file_name);
+
+    string line;
+    int max_length = 0;
+
+    while (getline(input_file, line))
     {
-        if (line1 != line2)
+        int length = line.length();
+        if (length > max_length)
         {
-            cout << "Lines at line number " << lineNumber << " don't match:\n"
-                << "File 1: " << line1 << '\n'
-                << "File 2: " << line2 << '\n';
-            return 0;
+            max_length = length;
         }
-        ++lineNumber;
     }
 
-    if (std::getline(file1, line1))
-    {
-        cout << "File 1 has additional lines starting at line number " << lineNumber << ":\n"
-            << line1 << '\n';
-    }
+    input_file.close();
 
-    if (getline(file2, line2))
-    {
-        std::cout << "File 2 has additional lines starting at line number " << lineNumber << ":\n"
-            << line2 << '\n';
-    }
+    cout << "Max length: " << max_length << endl;
 
     return 0;
 }
