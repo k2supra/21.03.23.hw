e0858d5ff0ecbcf858f4342476964b398c146f61
k2supra <andrijkorolevic@gmail.com>
2023-03-26 17:07:57 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,39 +1,34 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-
 using namespace std;
+
+
+string encrypt(string text)
+{
+    int key;
+    cout << "Enter key: ";
+    cin >> key;
+    string result = "";
+
+    for (int i = 0; i < text.length(); i++)
+    {
+        char c = text[i] + key;
+        if (c > 'z')
+            c = 'a' + (c - 'z' - 1);
+        result += c;
+    }
+    return result;
+}
 
 int main()
 {
-    ifstream file1("file1.txt");
-    ifstream file2("file2.txt");
-    string line1, line2;
-    int lineNumber = 1;
+    string message;
+    cout << "Enter message to encrypt: ";
+    getline(cin, message);
+    
 
-    while (getline(file1, line1) && getline(file2, line2))
-    {
-        if (line1 != line2)
-        {
-            cout << "Lines at line number " << lineNumber << " don't match:\n"
-                << "File 1: " << line1 << '\n'
-                << "File 2: " << line2 << '\n';
-            return 0;
-        }
-        ++lineNumber;
-    }
-
-    if (std::getline(file1, line1))
-    {
-        cout << "File 1 has additional lines starting at line number " << lineNumber << ":\n"
-            << line1 << '\n';
-    }
-
-    if (getline(file2, line2))
-    {
-        std::cout << "File 2 has additional lines starting at line number " << lineNumber << ":\n"
-            << line2 << '\n';
-    }
+    string encrypted_message = encrypt(message);
+    cout << "Encrypted message: " << encrypted_message << endl;
 
     return 0;
 }
