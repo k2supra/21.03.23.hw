3a1fa57d742c6b99316571ad76813c8ddbdd3334
k2supra <andrijkorolevic@gmail.com>
2023-03-26 17:13:45 +0300

    .

--- main.cpp
+++ main.cpp
@@ -24,14 +24,16 @@ string encrypt(string text)
 int main()
 {
     ifstream read("file__1.txt");
+    ofstream write("file__2.txt");
     string message;
     getline(read, message);
-    
 
     string encrypted_message = encrypt(message);
     cout << "Encrypted message: " << encrypted_message << endl;
+    write << "Encrypted message: " << encrypted_message << endl;
 
     read.close();
+    write.close();
 
     return 0;
 }
